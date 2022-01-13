48fa730c068cde1b7b44f8ebf310206f2b001ca9
Dean <deanscottg@gmail.com>
2022-01-13 14:27:43 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solutions in this file
+function writeCards(giftArray,event){
+    const namesArray=[];
+     for (let i = 0; i < giftArray.length; i++){
+       namesArray.push(`Thank you, ${giftArray[i]}, for the wonderful ${event} gift!`)
+     };
+     return namesArray;
+    }
+    function countDown(number){
+        for (let i = number; i >= 0; i--){
+           console.log(i);
+        }
+    }
